7197b9640a71744e96117ed5f14a1e9035bf50a0
Sebastian Cueva <sebastian.cueva@hgrinc.com>
2023-03-10 16:46:01 -0500

    Added credit card component

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -1,6 +1,8 @@
 import { React } from "react";
 import { CssVarsProvider } from "@mui/joy/styles";
 import Stack from "@mui/joy/Stack";
+import AppBar from "@components/common/AppBar";
+import CreditCard from "@components/homepage/CreditCard";
 
 export default function Login() {
   return (
@@ -9,24 +11,35 @@ export default function Login() {
         <Stack
           sx={{
             minHeight: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
             backgroundColor: "#ebedf0",
+            alignContent: "center",
           }}
         >
+          <AppBar />
           <Stack
+            padding="50px 5px 0px 5px"
+            spacing={1}
+            direction="column"
             sx={{
-              borderRadius: "sm",
-              padding: 5,
-              width: "100%",
-              maxWidth: "300px",
-              boxShadow: "var(--shadow)",
-              backgroundColor: "white",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
-            spacing={2}
-            variant="outlined"
-          />
+          >
+
+            <CreditCard
+              name="MasterCard"
+              quota="$3000"
+              expended="$16.51"
+              daysUntilPayment="14"
+            />
+            <CreditCard
+              name="Dinners Club"
+              quota="$500"
+              expended="$248.67"
+              daysUntilPayment="4"
+            />
+          </Stack>
         </Stack>
       </main>
     </CssVarsProvider>
