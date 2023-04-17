3d8271a1877d79c3a0b954d24141e0daa25d26a4
Sebastian Cueva <sebastian.cueva@hgrinc.com>
2023-04-16 20:56:03 -0500

    Added sliders

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -1,21 +1,158 @@
-import { React } from "react";
-import { CssVarsProvider } from "@mui/joy/styles";
-import Stack from "@mui/joy/Stack";
+import {
+  Button, Slider, Stack, Typography,
+} from "@mui/joy";
+import { React, useState } from "react";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
+import AllInclusiveIcon from "@mui/icons-material/AllInclusive";
 
 export default function Login() {
+  const [value, setValue] = useState([0, 100]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <CssVarsProvider>
-      <main>
+    <Stack
+      spacing={1}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "3vh",
+        height: "95vh",
+        boxShadow: "0px 1px 3px 0px #888888",
+        borderRadius: "6px",
+        backgroundImage: "linear-gradient(to bottom right, #2c3e50, #34495e)",
+        // filter: "blur(5px)",
+      }}
+    >
+      <Stack>
         <Stack
           sx={{
-            minHeight: "100vh",
-            backgroundColor: "#ebedf0",
-            alignContent: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            mb: "1.5vh",
           }}
         >
-          c
+          <Typography
+            level="h2"
+            fontSize="xl"
+            sx={{
+              color: "white",
+            }}
+          >
+            Song
+          </Typography>
+          <Typography
+            level="body1"
+            fontSize="md"
+            sx={{
+              color: "gray",
+            }}
+          >
+            Artist
+          </Typography>
         </Stack>
-      </main>
-    </CssVarsProvider>
+        <Stack
+          direction="row"
+          spacing={0.5}
+          mb="2vh"
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            variant="outlined"
+            color="success"
+          >
+            <SkipPreviousIcon />
+          </Button>
+          <Button
+            variant="outlined"
+            color="success"
+          >
+            <PlayArrowIcon />
+          </Button>
+          <Button
+            variant="outlined"
+            color="success"
+          >
+            <SkipNextIcon />
+          </Button>
+        </Stack>
+        <Stack width="30vh">
+          <Stack
+            spacing={1}
+            direction="row"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Slider
+              color="success"
+              valueLabelDisplay="off"
+              sx={{
+                paddingBottom: "0",
+              }}
+            />
+            <LibraryMusicIcon
+              sx={{
+                color: "white",
+              }}
+            />
+          </Stack>
+          <Stack
+            spacing={1}
+            direction="row"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Slider
+              color="info"
+            />
+            <VolumeUpIcon sx={{
+              color: "white",
+            }}
+            />
+          </Stack>
+          <Stack
+            spacing={1}
+            direction="row"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Slider
+              color="warning"
+              value={value}
+              onChange={handleChange}
+              sx={{
+                paddingBottom: "0px",
+              }}
+            />
+            <AllInclusiveIcon
+              sx={{
+                color: "white",
+              }}
+            />
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 }
